cf7c0c5bbd802c6b555c5ffc02d5e9d34adb84f1
李章源 <953251815@qq.com>
2023-10-19 15:54:00 +0800

    代理

--- vue.config.js
+++ vue.config.js
@@ -5,33 +5,33 @@ module.exports = defineConfig({
   outputDir: 'InspectorSys',
   transpileDependencies: true,
   devServer: {
-    host: "localhost",
+    host: "0.0.0.0",
     port: 80,
     open: false,
     proxy: {
       [process.env.VUE_APP_BASE_API]: {
-        // target: "http://localhost:8080",
-        target: "https://cyhdl1ttleyuming.cn/wx2/HZ26-6/",
+        target: "http://127.0.0.1:8080",
+        // target: "https://cyhdl1ttleyuming.cn/wx2/HZ26-6/",
         changeOrigin: true,
         pathRewrite: {
           ["^" + process.env.VUE_APP_BASE_API]: "",
         },
       },
-      "/dev-api": {
-        // target: "http://localhost:8080",
-        target: "https://cyhdl1ttleyuming.cn/wx2/HZ26-6/", //代理地址
-        changeOrigin: true,
-        pathRewrite: {
-          "^/dev-api": "",
-        },
-      },
-      "/accept-prod-api": {
-        target: "https://cyhdl1ttleyuming.cn/wx2/HZ26-6/", //代理地址
-        changeOrigin: true,
-        pathRewrite: {
-          "^/accept-prod-api": "",
-        },
-      },
+      // "/dev-api": {
+      //   // target: "http://localhost:8080",
+      //   target: "https://cyhdl1ttleyuming.cn/wx2/HZ26-6/", //代理地址
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     "^/dev-api": "",
+      //   },
+      // },
+      // "/accept-prod-api": {
+      //   target: "https://cyhdl1ttleyuming.cn/wx2/HZ26-6/", //代理地址
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     "^/accept-prod-api": "",
+      //   },
+      // },
     },
   },
 });
